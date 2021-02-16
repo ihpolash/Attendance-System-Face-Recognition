02434b9622e859144a25da0c0e9fa59fdfdae331
Ismail Hossain Polas <ismailhossainpolash@gmail.com>
2021-02-16 20:46:05 +0600

    Update settings.py

--- attendance_system_facial_recognition/settings.py
+++ attendance_system_facial_recognition/settings.py
@@ -136,12 +136,12 @@ LOGIN_REDIRECT_URL='dashboard'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'recognition', 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR,'recognition', 'static'),
 )
 
 # Simplified static file serving.
