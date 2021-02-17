6206f57662762b2c7fa7f5c4ccad74b107f058cb
Ismail Hossain Polas <ismail@shurjomukhi.com.bd>
2021-02-17 17:03:53 +0600

    updated new

--- recognition/views.py
+++ recognition/views.py
@@ -572,7 +572,33 @@ def add_photos(request):
 			form=usernameForm()
 			return render(request,'recognition/add_photos.html', {'form' : form})
 
+class VideoCamera(object):
+    def __init__(self):
+        # Using OpenCV to capture from device 0. If you have trouble capturing
+        # from a webcam, comment the line below out and use a video file
+        # instead.
+        self.video = cv2.VideoCapture(0)
+        # If you decide to use video.mp4, you must have this file in the folder
+        # as the main.py.
+        # self.video = cv2.VideoCapture('video.mp4')
+    
+    def __del__(self):
+        self.video.release()
+    
+    def get_frame(self):
+        success, image = self.video.read()
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
 
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 def mark_your_attendance(request):
 	
@@ -613,6 +639,8 @@ def mark_your_attendance(request):
 	while(True):
 		
 		frame = vs.read()
+
+		# frame = gen(camera)
 		
 		frame = imutils.resize(frame ,width = 800)
 		# frame = cv2.imread(frame)
@@ -620,7 +648,6 @@ def mark_your_attendance(request):
 		gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		
 		faces = detector(gray_frame,0)
-		
 		
 
 
